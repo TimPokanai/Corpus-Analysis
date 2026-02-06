2846cfd3917131f17d7b9fb1b5eedaf7ed1c21f3
Timothy Pokanai <pokanait@mcmaster.ca>
2026-02-05 22:01:08 -0500

    Modified preprocess.py for foreseeable final version

--- src/preprocess.py
+++ src/preprocess.py
@@ -6,6 +6,7 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 
+# Downloads for nltk word_tokenize() and list of stopwords
 nltk.download("punkt", quiet=True)
 nltk.download("punkt_tab", quiet=True)
 nltk.download("stopwords", quiet=True)
@@ -14,34 +15,25 @@ STOP_WORDS = set(stopwords.words("english"))
 STEM = PorterStemmer()
 
 def preprocess_document(text: str, use_stemming: bool = False) -> List[str]:
-    
-    # Lowercase the text
     text = text.lower()
-
-    # Tokenize the text
     tokens = word_tokenize(text)
-
-    # Keeping alphabetic tokens
     tokens = [t for t in tokens if t.isalpha()]
-
-    # Removing stopwords
     tokens = [t for t in tokens if t not in STOP_WORDS]
 
-    # Optional stemming
     if use_stemming:
         tokens = [STEM.stem(t) for t in tokens]
 
     return tokens
 
 def preprocess_corpus(documents: List[str], use_stemming: bool = False) -> List[List[str]]:
-    return [
-        preprocess_document(document, use_stemming=use_stemming)
-        for document in documents
-    ]
+    return [preprocess_document(document, use_stemming=use_stemming) for document in documents]
+
+# We pass the boolean arg use_stemming downstream through function calls 
+# to control whether stemming is applied for experimentation
 
 def preprocess_documents_by_category(
         grouped_docs: dict[str, List[str]], use_stemming: bool = False
-        ) -> dict[str, List[List[str]]]:
+    ) -> dict[str, List[List[str]]]:
     
     processed = {}
 
